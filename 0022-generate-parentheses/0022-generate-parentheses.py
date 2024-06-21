156fe7b73053c82f66be4da782ddbfc9e6eb8179
PratyushPriyamKuanr271776508 <37260459+PratyushPriyamKuanr271776508@users.noreply.github.com>
2024-06-21 11:21:09 +0530

    Time: 37 ms (60.60%), Space: 17 MB (11.83%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,16 +1,25 @@
 class Solution(object):
     def generateParenthesis(self, n):
         
-        def recurse(left = 0, right = 0, ls = [], s = ""):
-            if left == right == n:
-                ls.append(s)
-                return
+        dp = collections.defaultdict(list)
+        dp[(0, 0)] = [""]
+        
+        for left in range(1, n+1):
+            for right in range(left + 1):
+                if left > right: dp[(left, right)] += [i + "(" for i in dp[(left-1, right)]]
+                if right > 0: dp[(left, right)] += [i + ")" for i in dp[(left, right-1)]]
+        return dp[(n, n)]
+        
+#         def recurse(left = 0, right = 0, ls = [], s = ""):
+#             if left == right == n:
+#                 ls.append(s)
+#                 return
             
-            if left < n: recurse(left + 1, right, ls, s + '(')
-            if right < left: recurse(left, right + 1, ls, s + ')')
+#             if left < n: recurse(left + 1, right, ls, s + '(')
+#             if right < left: recurse(left, right + 1, ls, s + ')')
             
-            return ls
-        return recurse()
+#             return ls
+        # return recurse()
 
 
 
